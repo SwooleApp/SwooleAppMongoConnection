b56060bfb56c103b3c30be6d94744db0d21ca33a
SidorkinAlex <palach.orl@mail.ru>
2024-08-21 23:04:38 +0300

    add comment from creating pool

--- src/Initializers/MongoPoolInitializer.php
+++ src/Initializers/MongoPoolInitializer.php
@@ -3,5 +3,6 @@
 namespace SwooleApp\SwooleAppMongoConnection\Initializers;
 class MongoPoolInitializer
 {
+    //todo сделать инициализацию пула в случае если требуется создать пулл конекшенов , а в слуае если инициализация подключения происходит в рантайме то не делать эту инициализацию. и несоздавать пул.
 
 }

c198f8ae50e60c3cd42e999b5cc3bf6b475b2861
SidorkinAlex <palach.orl@mail.ru>
2024-11-02 08:18:21 +0300

    add Task Executor Mongo metods from static connettion

--- src/Tasks/MongoTasksExecutor.php
+++ src/Tasks/MongoTasksExecutor.php
@@ -71,43 +71,6 @@ class MongoTasksExecutor extends AbstractTaskExecutor
     }
 
     /**
-     * @return mixed[]
-     */
-    private function commandStaticExec(): array
-    {
-        $option = $this->dataStorage['option'] ?? [];
-        switch ($this->dataStorage['method']) {
-            case 'find':
-                $result = $this->findStatic($this->dataStorage['collectionName'], $this->dataStorage['query'], $option);
-                break;
-//            case 'findOne':
-//                $result = $this->findOne($this->dataStorage['collectionName'], $this->dataStorage['query'], $option);
-//                break;
-//            case 'insertOne':
-//                $result = $this->insertOne($this->dataStorage['collectionName'], $this->dataStorage['data'], $option);
-//                break;
-//            case 'insertMany':
-//                $result = $this->insertMany($this->dataStorage['collectionName'], $this->dataStorage['data'], $option);
-//                break;
-//            case 'updateOne':
-//                $result = $this->updateOne($this->dataStorage['collectionName'], $this->dataStorage['query'], $this->dataStorage['updateData'], $option);
-//                break;
-//            case 'updateMany':
-//                $result = $this->updateMany($this->dataStorage['collectionName'], $this->dataStorage['query'], $this->dataStorage['updateData'], $option);
-//                break;
-//            case 'deleteOne':
-//                $result = $this->deleteOne($this->dataStorage['collectionName'], $this->dataStorage['query'], $option);
-//                break;
-//            case 'deleteMany':
-//                $result = $this->deleteMany($this->dataStorage['collectionName'], $this->dataStorage['query'], $option);
-//                break;
-            default:
-                throw new \Exception('Unsupported method: ' . $this->dataStorage['method']);
-        }
-        return $result;
-    }
-
-    /**
      * @param string $collectionName
      * @param string $poolKey
      * @param array<mixed>|object $query
@@ -124,25 +87,6 @@ class MongoTasksExecutor extends AbstractTaskExecutor
         unset($mongoPool);
         return $result;
     }
-
-    /**
-     * @param string $collectionName
-     * @param array<mixed>|object $query
-     * @param array<mixed> $option
-     * @return array<mixed>
-     * @throws \Exception
-     */
-    protected
-    function findStatic(string $collectionName, array|object $query, array $option = []): array
-    {
-        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
-        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
-        $result = $mongoClient->selectCollection($collectionName)->find($query, $option)->toArray();
-        unset($mongoClient);
-        unset($mongoConfig);
-        return $result;
-    }
-
     /**
      * @param string $poolKey
      * @param string $collectionName
@@ -171,4 +115,248 @@ class MongoTasksExecutor extends AbstractTaskExecutor
             throw new \Exception('container with key ' . $poolKey . 'is not a MongoPool instance');
         }
     }
+
+
+    /**
+     * @return mixed[]
+     */
+    private function commandStaticExec(): array
+    {
+        $option = $this->dataStorage['option'] ?? [];
+        switch ($this->dataStorage['method']) {
+            case 'find':
+                $result = $this->find($this->dataStorage['collectionName'], $this->dataStorage['query'], $option);
+                break;
+
+            case 'findOne':
+                $result = $this->findOne($this->dataStorage['collectionName'], $this->dataStorage['query'], $option);
+                break;
+
+            case 'insertOne':
+                $result = $this->insertOne($this->dataStorage['collectionName'], $this->dataStorage['data'], $option);
+                break;
+
+            case 'insertMany':
+                $result = $this->insertMany($this->dataStorage['collectionName'], $this->dataStorage['data'], $option);
+                break;
+
+            case 'updateOne':
+                $result = $this->updateOne($this->dataStorage['collectionName'], $this->dataStorage['filter'], $this->dataStorage['update'], $option);
+                break;
+
+            case 'updateMany':
+                $result = $this->updateMany($this->dataStorage['collectionName'], $this->dataStorage['filter'], $this->dataStorage['update'], $option);
+                break;
+
+            case 'deleteOne':
+                $result = $this->deleteOne($this->dataStorage['collectionName'], $this->dataStorage['filter'], $option);
+                break;
+
+            case 'deleteMany':
+                $result = $this->deleteMany($this->dataStorage['collectionName'], $this->dataStorage['filter'], $option);
+                break;
+
+            default:
+                throw new \Exception('Unsupported method: ' . $this->dataStorage['method']);
+        }
+        return $result;
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array<mixed>|object $query
+     * @param array<mixed> $option
+     * @return array<mixed>
+     * @throws \Exception
+     */
+    protected function find(string $collectionName, array|object $query, array $option = []): array
+    {
+        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
+        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
+
+        // Find multiple documents
+        $result = $mongoClient->selectCollection($collectionName)->find($query, $option)->toArray();
+
+        unset($mongoClient);
+        unset($mongoConfig);
+
+        return $result;
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array<mixed>|object $query
+     * @param array<mixed> $option
+     * @return mixed|null
+     * @throws \Exception
+     */
+    protected function findOne(string $collectionName, array|object $query, array $option = []): mixed
+    {
+        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
+        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
+
+        // Find a single document
+        $result = $mongoClient->selectCollection($collectionName)->findOne($query, $option);
+
+        unset($mongoClient);
+        unset($mongoConfig);
+
+        return $result;
+    }
+    /**
+     * @param string $collectionName
+     * @param array<mixed>|object $data
+     * @param array<mixed> $option
+     * @return array<mixed>
+     * @throws \Exception
+     */
+    protected function insertOne(string $collectionName, array|object $data, array $option = []): array
+    {
+        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
+        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
+
+        // Insert the document
+        $result = $mongoClient->selectCollection($collectionName)->insertOne($data, $option);
+
+        unset($mongoClient);
+        unset($mongoConfig);
+
+        // Return the result as an array
+        return [
+            'insertedId' => (string)$result->getInsertedId(),
+            'acknowledged' => $result->isAcknowledged(),
+        ];
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array<array<mixed>|object> $data
+     * @param array<mixed> $option
+     * @return array<mixed>
+     * @throws \Exception
+     */
+    protected function insertMany(string $collectionName, array $data, array $option = []): array
+    {
+        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
+        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
+
+        // Insert multiple documents
+        $result = $mongoClient->selectCollection($collectionName)->insertMany($data, $option);
+
+        unset($mongoClient);
+        unset($mongoConfig);
+
+        // Return the result as an array
+        $insertedIds = array_map(fn($id) => (string)$id, $result->getInsertedIds());
+
+        return [
+            'insertedIds' => $insertedIds,
+            'acknowledged' => $result->isAcknowledged(),
+        ];
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array<mixed> $filter
+     * @param array<mixed> $update
+     * @param array<mixed> $option
+     * @return array<mixed>
+     * @throws \Exception
+     */
+    protected function updateOne(string $collectionName, array $filter, array $update, array $option = []): array
+    {
+        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
+        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
+
+        // Update a single document
+        $result = $mongoClient->selectCollection($collectionName)->updateOne($filter, $update, $option);
+
+        unset($mongoClient);
+        unset($mongoConfig);
+
+        // Return the result as an array
+        return [
+            'matchedCount' => $result->getMatchedCount(),
+            'modifiedCount' => $result->getModifiedCount(),
+            'acknowledged' => $result->isAcknowledged(),
+        ];
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array<mixed> $filter
+     * @param array<mixed> $update
+     * @param array<mixed> $option
+     * @return array<mixed>
+     * @throws \Exception
+     */
+    protected function updateMany(string $collectionName, array $filter, array $update, array $option = []): array
+    {
+        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
+        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
+
+        // Update multiple documents
+        $result = $mongoClient->selectCollection($collectionName)->updateMany($filter, $update, $option);
+
+        unset($mongoClient);
+        unset($mongoConfig);
+
+        // Return the result as an array
+        return [
+            'matchedCount' => $result->getMatchedCount(),
+            'modifiedCount' => $result->getModifiedCount(),
+            'acknowledged' => $result->isAcknowledged(),
+        ];
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array<mixed> $filter
+     * @param array<mixed> $option
+     * @return array<mixed>
+     * @throws \Exception
+     */
+    protected function deleteOne(string $collectionName, array $filter, array $option = []): array
+    {
+        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
+        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
+
+        // Delete a single document
+        $result = $mongoClient->selectCollection($collectionName)->deleteOne($filter, $option);
+
+        unset($mongoClient);
+        unset($mongoConfig);
+
+        // Return the result as an array
+        return [
+            'deletedCount' => $result->getDeletedCount(),
+            'acknowledged' => $result->isAcknowledged(),
+        ];
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array<mixed> $filter
+     * @param array<mixed> $option
+     * @return array<mixed>
+     * @throws \Exception
+     */
+    protected function deleteMany(string $collectionName, array $filter, array $option = []): array
+    {
+        $mongoConfig = (new MongoConfigGetter())->getValidConfig($this->app);
+        $mongoClient = (new MongoStaticClientInitializer)->getMongoClient($mongoConfig->connectionCredential);
+
+        // Delete multiple documents
+        $result = $mongoClient->selectCollection($collectionName)->deleteMany($filter, $option);
+
+        unset($mongoClient);
+        unset($mongoConfig);
+
+        // Return the result as an array
+        return [
+            'deletedCount' => $result->getDeletedCount(),
+            'acknowledged' => $result->isAcknowledged(),
+        ];
+    }
+
 }

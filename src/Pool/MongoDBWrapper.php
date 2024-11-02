e7eb9b6141f9808b7af47f1dded4d1be381dd851
SidorkinAlex <palach.orl@mail.ru>
2024-11-02 07:37:05 +0300

    add methods from MongoDb Write

--- src/Pool/MongoDBWrapper.php
+++ src/Pool/MongoDBWrapper.php
@@ -29,16 +29,16 @@ class MongoDBWrapper
      * @phpstan-ignore-next-line
      * @throws TaskException
      */
-    public function insertOne(string $collectionName, string|null $poolKey, array|object $data, array $option = [], float $timeout = 0): array
+    public function insertOne(string|null $poolKey, string $collectionName, array|object $data, array $option = [], float $timeout = 0): array
     {
-        $taskData = new BasicTaskData('Sidalex\TestSwoole\Tasks\TestTaskExecutorPool', [
+        $taskData = new BasicTaskData('SwooleApp\SwooleAppMongoConnection\Tasks\MongoTasksExecutor', [
             'method' => 'insertOne',
             'poolKey' => $poolKey,
             'collectionName' => $collectionName,
             'data' => $data,
             'option' => $option,
         ]);
-        $taskResult = $this->server->taskwait($taskData,$timeout);
+        $taskResult = $this->server->taskwait($taskData, $timeout);
         if (
             ($taskResult instanceof TaskResulted) &&
             is_array($taskResult->getResult())
@@ -58,7 +58,7 @@ class MongoDBWrapper
      * @return int
      * @throws DataException
      */
-    public function insertOneAsync(string $collectionName, string|null $poolKey, array|object $data, array $option = []) :int
+    public function insertOneAsync(string|null $poolKey, string $collectionName, array|object $data, array $option = []): int
     {
         $taskData = new BasicTaskData('Sidalex\TestSwoole\Tasks\TestTaskExecutorPool', [
             'method' => 'insertOne',
@@ -73,37 +73,358 @@ class MongoDBWrapper
         ) {
             return $taskResult;
         } else {
-            throw new DataException('Error in task async execution: $taskResult='.$taskResult);
+            throw new DataException('Error in task async execution: $taskResult=' . $taskResult);
         }
     }
 
-//    public function insertMany(string $collectionName, array|object $data, array $option = []):array
-//    {
-//    }
-//    public function insertManyAsync(string $collectionName, array|object $data, array $option = []): int
-//    {
-//    }
-//
-//    public function updateOne(string $collectionName, array|object $query, array|object $updateData, array $option = []):array
-//    {
-//    }
-//    public function updateMany(string $collectionName, array|object $query, array|object $updateData, array $option = []):array
-//    {
-//    }
-//
-//    public function deleteOne(string $collectionName, array|object $query, array $option = []):array
-//    {
-//    }
-//    public function deleteMany(string $collectionName, array|object $query, array $option = []):array
-//    {
-//    }
-//
-//    public function findOne(string $collectionName, array|object $query, array $option = []):array
-//    {
-//    }
-//
-//    public function find(string $collectionName, array|object $query, array $option = []):array
-//    {
-//    }
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $update
+     * @param array<mixed> $option
+     * @param float $timeout time
+     * @return array<mixed>
+     * @throws DataException
+     */
+    public function updateOne(string|null $poolKey, string $collectionName, array $filter, array $update, array $option = [], float $timeout = 0): array
+    {
+        $taskData = new BasicTaskData('SwooleApp\SwooleAppMongoConnection\Tasks\MongoTasksExecutor', [
+            'method' => 'updateOne',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'update' => $update,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->taskwait($taskData, $timeout);
+        if (
+            ($taskResult instanceof TaskResulted) &&
+            is_array($taskResult->getResult())
+        ) {
+            return $taskResult->getResult();
+        } else {
+            throw new DataException('Error in task execution: data is not array');
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $update
+     * @param array<mixed> $option
+     * @return int
+     * @throws DataException
+     */
+    public function updateOneAsync(string|null $poolKey, string $collectionName, array $filter, array $update, array $option = []): int
+    {
+        $taskData = new BasicTaskData('Sidalex\TestSwoole\Tasks\TestTaskExecutorPool', [
+            'method' => 'updateOne',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'update' => $update,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->task($taskData);
+        if (
+            (is_int($taskResult))
+        ) {
+            return $taskResult;
+        } else {
+            throw new DataException('Error in task async execution: $taskResult=' . $taskResult);
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $option
+     * @param float $timeout time
+     * @return array<mixed>
+     * @throws DataException
+     */
+    public function deleteOne(string|null $poolKey, string $collectionName, array $filter, array $option = [], float $timeout = 0): array
+    {
+        $taskData = new BasicTaskData('SwooleApp\SwooleAppMongoConnection\Tasks\MongoTasksExecutor', [
+            'method' => 'deleteOne',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->taskwait($taskData, $timeout);
+        if (
+            ($taskResult instanceof TaskResulted) &&
+            is_array($taskResult->getResult())
+        ) {
+            return $taskResult->getResult();
+        } else {
+            throw new DataException('Error in task execution: data is not array');
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $option
+     * @return int
+     * @throws DataException
+     */
+    public function deleteOneAsync(string|null $poolKey, string $collectionName, array $filter, array $option = []): int
+    {
+        $taskData = new BasicTaskData('Sidalex\TestSwoole\Tasks\TestTaskExecutorPool', [
+            'method' => 'deleteOne',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->task($taskData);
+        if (
+            (is_int($taskResult))
+        ) {
+            return $taskResult;
+        } else {
+            throw new DataException('Error in task async execution: $taskResult=' . $taskResult);
+        }
+    }
+
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<array<mixed>|object> $data
+     * @param array<mixed> $option
+     * @param float $timeout time
+     * @return array<mixed>
+     * @throws DataException
+     */
+    public function insertMany(string|null $poolKey, string $collectionName, array $data, array $option = [], float $timeout = 0): array
+    {
+        $taskData = new BasicTaskData('SwooleApp\SwooleAppMongoConnection\Tasks\MongoTasksExecutor', [
+            'method' => 'insertMany',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'data' => $data,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->taskwait($taskData, $timeout);
+        if (
+            ($taskResult instanceof TaskResulted) &&
+            is_array($taskResult->getResult())
+        ) {
+            //todo реализовать проверку валидности ответа
+            return $taskResult->getResult();
+        } else {
+            throw new DataException('Error in task execution: data is not array');
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<array<mixed>|object> $data
+     * @param array<mixed> $option
+     * @return int
+     * @throws DataException
+     */
+    public function insertManyAsync(string|null $poolKey, string $collectionName, array $data, array $option = []): int
+    {
+        $taskData = new BasicTaskData('Sidalex\TestSwoole\Tasks\TestTaskExecutorPool', [
+            'method' => 'insertMany',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'data' => $data,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->task($taskData);
+        if (
+            (is_int($taskResult))
+        ) {
+            return $taskResult;
+        } else {
+            throw new DataException('Error in task async execution: $taskResult=' . $taskResult);
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $update
+     * @param array<mixed> $option
+     * @param float $timeout time
+     * @return array<mixed>
+     * @throws DataException
+     */
+    public function updateMany(string|null $poolKey, string $collectionName, array $filter, array $update, array $option = [], float $timeout = 0): array
+    {
+        $taskData = new BasicTaskData('SwooleApp\SwooleAppMongoConnection\Tasks\MongoTasksExecutor', [
+            'method' => 'updateMany',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'update' => $update,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->taskwait($taskData, $timeout);
+        if (
+            ($taskResult instanceof TaskResulted) &&
+            is_array($taskResult->getResult())
+        ) {
+            //todo реализовать проверку валидности ответа
+            return $taskResult->getResult();
+        } else {
+            throw new DataException('Error in task execution: data is not array');
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $update
+     * @param array<mixed> $option
+     * @return int
+     * @throws DataException
+     */
+    public function updateManyAsync(string|null $poolKey, string $collectionName, array $filter, array $update, array $option = []): int
+    {
+        $taskData = new BasicTaskData('Sidalex\TestSwoole\Tasks\TestTaskExecutorPool', [
+            'method' => 'updateMany',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'update' => $update,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->task($taskData);
+        if (
+            (is_int($taskResult))
+        ) {
+            return $taskResult;
+        } else {
+            throw new DataException('Error in task async execution: $taskResult=' . $taskResult);
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $option
+     * @param float $timeout time
+     * @return array<mixed>
+     * @throws DataException
+     */
+    public function deleteMany(string|null $poolKey, string $collectionName, array $filter, array $option = [], float $timeout = 0): array
+    {
+        $taskData = new BasicTaskData('SwooleApp\SwooleAppMongoConnection\Tasks\MongoTasksExecutor', [
+            'method' => 'deleteMany',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->taskwait($taskData, $timeout);
+        if (
+            ($taskResult instanceof TaskResulted) &&
+            is_array($taskResult->getResult())
+        ) {
+            //todo реализовать проверку валидности ответа
+            return $taskResult->getResult();
+        } else {
+            throw new DataException('Error in task execution: data is not array');
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $option
+     * @return int
+     * @throws DataException
+     */
+    public function deleteManyAsync(string|null $poolKey, string $collectionName, array $filter, array $option = []): int
+    {
+        $taskData = new BasicTaskData('Sidalex\TestSwoole\Tasks\TestTaskExecutorPool', [
+            'method' => 'deleteMany',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->task($taskData);
+        if (
+            (is_int($taskResult))
+        ) {
+            return $taskResult;
+        } else {
+            throw new DataException('Error in task async execution: $taskResult=' . $taskResult);
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $option
+     * @param float $timeout time
+     * @return array<mixed>
+     * @throws DataException
+     */
+    public function findOne(string|null $poolKey, string $collectionName, array $filter, array $option = [], float $timeout = 0): array
+    {
+        $taskData = new BasicTaskData('SwooleApp\SwooleAppMongoConnection\Tasks\MongoTasksExecutor', [
+            'method' => 'findOne',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->taskwait($taskData, $timeout);
+        if (
+            ($taskResult instanceof TaskResulted) &&
+            is_array($taskResult->getResult())
+        ) {
+            return $taskResult->getResult();
+        } else {
+            throw new DataException('Error in task execution: data is not array');
+        }
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string|null $poolKey
+     * @param array<mixed> $filter
+     * @param array<mixed> $option
+     * @param float $timeout time
+     * @return array<array<mixed>>
+     * @throws DataException
+     */
+    public function find(string|null $poolKey, string $collectionName, array $filter = [], array $option = [], float $timeout = 0): array
+    {
+        $taskData = new BasicTaskData('SwooleApp\SwooleAppMongoConnection\Tasks\MongoTasksExecutor', [
+            'method' => 'find',
+            'poolKey' => $poolKey,
+            'collectionName' => $collectionName,
+            'filter' => $filter,
+            'option' => $option,
+        ]);
+        $taskResult = $this->server->taskwait($taskData, $timeout);
+        if (
+            ($taskResult instanceof TaskResulted) &&
+            is_array($taskResult->getResult())
+        ) {
+            return $taskResult->getResult();
+        } else {
+            throw new DataException('Error in task execution: data is not array');
+        }
+    }
 
 }
